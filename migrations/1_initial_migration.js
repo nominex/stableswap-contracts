1cc8e915e58c53b8e03d2875bc37505434f4f413
dev00 <dev00@nomiswap.io>
2021-12-20 17:34:14 +0400

    Updated dependencies; added truffle-config

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,6 @@
+const Migrations = artifacts.require('Migrations')
+
+module.exports = async function (deployer) {
+  // Deploy the Migrations contract as our only task
+  await deployer.deploy(Migrations)
+}
