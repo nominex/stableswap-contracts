b252f6c4396b83fd1a667771c5cd7bb1d62b6e0d
dev02 <dev02@nomiswap.io>
2021-12-28 16:10:00 +0400

    Migrations deployed to testnet

--- migrations/2_deploy_NomiswapFactory.js
+++ migrations/2_deploy_NomiswapFactory.js
@@ -4,7 +4,8 @@ module.exports = async function (deployer, network, accounts) {
   // Deploy the NomiswapFactory contract as our only task
   await deployer.deploy(NomiswapFactory, accounts[0])
 
-  const nomiswapFactoryInstance = await NomiswapFactory.deployed()
-
-  await nomiswapFactoryInstance.setFeeTo(accounts[0])
+  const nomiswapFactory = await NomiswapFactory.deployed()
+  console.log('NomiswapFactory deployed with ', await nomiswapFactory.INIT_CODE_HASH(), "init code hash");
+  const swapFeeReciever = '0x993439e65D78412B27568605CDadd04A3C582ddb';
+  await nomiswapFactory.setFeeTo(swapFeeReciever);
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,22 @@
+const Wallet = require('ethereumjs-wallet').default;
+const ProviderEngine = require("@trufflesuite/web3-provider-engine");
+const WalletSubprovider = require('@trufflesuite/web3-provider-engine/subproviders/wallet.js');
+const Web3Subprovider = require("@trufflesuite/web3-provider-engine/subproviders/provider.js");
+const web3 = require("web3");
+const nconf = require("nconf");
+
+nconf.argv().env().file({ file: './.config.json' });
+function provider(url) {
+  nconf.required(["DEPLOYER_PRIVATE_KEY"]);
+  const privateKey = nconf.get("DEPLOYER_PRIVATE_KEY");
+  const wallet = new Wallet(Buffer.from(privateKey, "hex"));
+  const engine = new ProviderEngine();
+  engine.addProvider(new WalletSubprovider(wallet, {}));
+  engine.addProvider(new Web3Subprovider(new web3.providers.HttpProvider(url, { keepAlive: true, timeout: 1000000 })));
+  engine.on = (block) => {}
+  engine.start();
+  return engine;
+}
 
 module.exports = {
   networks: {
@@ -6,6 +25,18 @@ module.exports = {
       port: 7545,
       network_id: "97",
       gas: 6721975,
+    },
+    testnet: {
+      network_id: "97",
+      provider: () => provider("https://data-seed-prebsc-1-s1.binance.org:8545/"),
+      networkCheckTimeout: 1000000,
+      gasPrice: 10000000000
+    },
+    mainnet: {
+      network_id: "56",
+      provider: () => provider("https://bsc-dataseed1.binance.org/"),
+      networkCheckTimeout: 1000000,
+      gasPrice: 5000000000
     }
   },
 
