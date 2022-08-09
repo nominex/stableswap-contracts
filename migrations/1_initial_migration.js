964e2a239a5964c51bc778231b389ffc5a2b8905
dev01 <dev01@nomiswap.io>
2022-08-09 09:26:58 +0400

    renamed package

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,5 +2,5 @@ const Migrations = artifacts.require('Migrations')
 
 module.exports = async function (deployer) {
   // Deploy the Migrations contract as our only task
-  await deployer.deploy(Migrations)
+  // await deployer.deploy(Migrations)
 }
