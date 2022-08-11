ec631e8ee2aa53bc6664bb8cf61417d67cb40020
dev01 <dev01@nomiswap.io>
2022-08-11 21:45:02 +0400

    fixed tests

--- test/StableSwapFactory.spec.ts
+++ test/StableSwapFactory.spec.ts
@@ -88,7 +88,7 @@ describe('NomiswapFactory', () => {
   it('createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES);
     const receipt = await tx.wait();
-    expect(receipt.gasUsed).to.eq(3472445)
+    expect(receipt.gasUsed).to.eq(3492931)
   });
 
   it('setFeeTo', async () => {
