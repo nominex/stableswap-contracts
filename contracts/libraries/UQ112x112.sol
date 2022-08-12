ae2944c5ddfbd613fdccefaaedcb3972b244ad66
dev01 <dev01@nomiswap.io>
2022-08-12 07:19:25 +0400

    reverted license back to gpl v3

--- contracts/libraries/UQ112x112.sol
+++ contracts/libraries/UQ112x112.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity =0.8.15;
 
 // a library for handling binary fixed point numbers (https://en.wikipedia.org/wiki/Q_(number_format))
